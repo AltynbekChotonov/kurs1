e6a68396f7f8fbbe7e53c02a261df396e35687a5
AltynbekChotonov <dtvkanalnews@gmail.com>
2023-07-31 23:48:23 +0600

    added sabak_kaitaloo_f9

---
+++ sabak_kaitaloo_f9/lib/user_data.dart
@@ -0,0 +1,41 @@
+import 'user_model.dart';
+import 'users.dart';
+
+final List<User> users = [
+  User(
+      atyjonu: "Бакыт Жунушов",
+      kesibi: 'Flutter Developer',
+      jash: 50,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+  User(
+      atyjonu: "Cамара Стамбекова",
+      kesibi: 'Flutter Developer',
+      jash: 44,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+  User(
+      atyjonu: "Наргиза Кульмамырова",
+      kesibi: 'Flutter Developer',
+      jash: 36,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+  User(
+      atyjonu: "Нуркадыр Асанбек уулу",
+      kesibi: 'Flutter Developer',
+      jash: 25,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+  User(
+      atyjonu: "Али Шамсудинов",
+      kesibi: 'Flutter Developer',
+      jash: 15,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+  User(
+      atyjonu: "Элдияр Эгембердиев",
+      kesibi: 'Flutter Developer',
+      jash: 25,
+      prosurot: 'https://picsum.photos/seed/picsum/200/300',
+      tajriybajolu: TajText),
+];
---
+++ sabak_kaitaloo_f9/lib/users.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/material.dart';
+
+import 'user_data.dart';
+// <> Generics  === belgiluu tip
+
+// ignore: constant_identifier_names
+const String TajText =
+    'What is Lorem Ipsum? Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
+
+class UsersPage extends StatefulWidget {
+  const UsersPage({super.key});
+
+  @override
+  State<UsersPage> createState() => _UsersPageState();
+}
+
+class _UsersPageState extends State<UsersPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('User Page'),
+      ),
+      body: ListView.builder(
+          padding: const EdgeInsets.all(8),
+          itemCount: users.length,
+          itemBuilder: (context, index) {
+            final User = users[index];
+            return ListTile(
+              title: Text(users[index].atyjonu),
+              subtitle: Text('${User.kesibi} ${User.jash} jashta'),
+              leading: CircleAvatar(
+                radius: 30,
+                backgroundImage: NetworkImage(User.prosurot),
+              ),
+            );
+          }),
+    );
+  }
+}
